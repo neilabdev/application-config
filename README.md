b1334cc9ccfaf91c323291f4161c9a369e91d6f2
ghost <James.Whitfield@JadeMind.com>
2021-01-30 13:01:24 -0500

    reverted to 3.3.12

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 Application-Config
 ===============
+<<<<<<< HEAD
 [![Build Status](https://travis-ci.org/neilabdev/application-config.svg?branch=master)](https://travis-ci.org/neilabdev/application-config)
+=======
+[![Build Status](https://travis-ci.org/sbglasius/external-config.svg?branch=master)](https://travis-ci.org/sbglasius/external-config)
+
+This plugin will mimic the Grails 2 way of handling external configurations defined in `grails.config.locations`.
+
+VERSIONS!
+---------
+The `grails-3.x` branch is for Grails 3.x, `master` will, moving forward be for Grails 4.x
+
+IMPORTANT!
+----------
+The External Config Plugin (1.1.0 and above) no longer needs to implement `ExternalConfig` on `Application.groovy`. It now uses a `SpringApplicationRunListener`and hooks into the startup automagically. So if you used the plugin in prior versions, please remove `implements ExternalConfig` from `Application.groovy`
+>>>>>>> external-config/master
 
 This plugin based on the amazing work started in plugin  [external-config](http://plugins.grails.org/plugin/grails/external-config) by [Sudhir Nimavat](https://github.com/snimavat) and  [Dennie de Lange](https://github.com/tkvw),
  which mimiced the Grails 2 way of handling external configurations defined in `grails.config.locations`; with the necessary additions to allow configurations via command line, system properties, and JNDI.
@@ -8,7 +22,17 @@ This plugin based on the amazing work started in plugin  [external-config](http:
 Contributors
 ------------
 
+<<<<<<< HEAD
 * [neilabdev](https://github.com/neilabdev)
+=======
+Major contributors
+
+* [Jesper Steen Møller](https://github.com/jespersm)
+* [Tucker J. Pelletier](https://github.com/virtualdogbert)
+* [Dennie de Lange](https://github.com/tkvw)
+* [Sudhir Nimavat](https://github.com/snimavat) 
+* [Anders Aaberg](https://github.com/andersaaberg)
+>>>>>>> external-config/master
 
 
 Installation
@@ -18,7 +42,27 @@ Add dependency to your `build.gradle`:
 
 ```
 dependencies {
+<<<<<<< HEAD
     compile 'com.neilab.plugins:application-config:1.1.3'
+=======
+    compile 'org.grails.plugins:external-config:1.2.2'
+}
+```
+
+To use a snapshot-version
+
+add JFrog OSS Repository to the `repositories`:
+```
+repositories {
+    maven { url "https://oss.jfrog.org/repo/" }
+}
+```
+
+and specify the snapshot version as a dependency:
+```
+dependencies {
+    compile 'org.grails.plugins:external-config:1.3.0.BUILD-SNAPSHOT'
+>>>>>>> external-config/master
 }
 ```
 
@@ -64,6 +108,7 @@ grails.config.locations = [
 ]
 ```
 
+<<<<<<< HEAD
 You may also include external configs using '-D' arguments which match the system properties
 the application is seeking, preceded by an application prefix determined by *info.app.name* in your default *application.yml*, *application.groovy*  or "app" if none exists. By default the following should work:
 
@@ -106,13 +151,66 @@ This above is necessary only when *ApplicationConfigRunListener* is not executed
 Notes
 -----
 Notice, that `~/` references the users `$HOME` directory.
+=======
+It is also possible to define it in an environment specific block (groovy):
+```$xslt
+environments {
+    test {
+        grails {
+            config {
+                locations = [...]
+            }
+        }
+    }
+}   
+```
+
+or (yml)
+
+```
+environments:
+    test:
+        grails:
+            config:
+                locations:
+                - ... 
+```
+
+`~/` references the users `$HOME` directory.
+>>>>>>> external-config/master
 Notice, that using a system property you should use single quotes because otherwise it's interpreted as a Gstring.
 
 The plugin will skip configuration files that are not found. 
 
 For `.groovy` and `.yml` files the `environments` blocks in the config file are interpreted the same way, as in `application.yml` or `application.groovy`.
 
+<<<<<<< HEAD
 Alternatively, you can make a gradle script to move the external configuration file to your classpath (e.g. /build/classes)
+=======
+**Wildcard support**
+
+It is possible to use `*` as wildcards in the filename part of the configuration:
+
+```
+grails:
+    config:
+        locations:
+            - file:/etc/app/myconfig*.groovy
+            - ~/.grails/myconfig*.groovy
+```
+or
+```
+grails.config.locations = [
+        "file:/etc/app/myconfig*.groovy",
+        "~/.grails/myconfig*.groovy",
+]
+```
+__Note__: that it only works for the `file:` and `~/` prefix. 
+
+__Note__: the wildcards are in the order they are found in the `locations` list, but the order of the expanded `locations` for each wildcard is not guaranteed, and is dependent on the OS used.
+
+**Getting configuration from another folder than /conf on classpath without moving it with Gradle script**
+>>>>>>> external-config/master
 
 **Using IntelliJ or gradle to specify configurations via system properties**
 
@@ -132,8 +230,26 @@ and one for converting groovy config to yml config. These scripts are not guaran
 perfect, but you should report any edge cases for the yml to groovy config here:
 https://github.com/virtualdogbert/GroovyConfigWriter/issues
 
+grails yml-to-groovy-config has the following parameters:
+* ymlFile - The yml input file.
+* asClosure - An optional flag to set the output to be closure based or map based. The Default is closure based 
+* outputFile - The optional output file. If none is provided, then the output will go to System.out.
+* indent - Optional indent. The default is 4 spaces
+* escapeList - An optional CSV list of values to escape, with no spaces. The default is 'default'
+
+
 Sample usage:
 ```
-grails yml-to-groovy-config [ymlFile] [optional outputFile]
-grails groovy-to-yml-config [ymlFile] [optional outputFile]
+grails yml-to-groovy-config [ymlFile] [optional asClosure] [optional outputFile] [optional indent] [optional escape list]
+```
+
+grails groovy-to-yml-config has the following parameters:
+* groovy - The groovy input file.'
+* outputFile - The optional output file. If none is provided, then the output will go to System.out.
+* indent' - Sets the optional indent level for a file. The default is 4
+* flow - Sets the optional style of BLOCK or FLOWS. The default is BLOCK.
+
+sample usage:
+```
+grails groovy-to-yml-config [ymlFile] [optional outputFile] [optional indent] [optinal flow]
 ```
---
+++ external-config/src/main/groovy/com/neilab/plugins/config/ExternallyConfigurable.groovy
@@ -0,0 +1,237 @@
+package com.neilab.plugins.config
+
+
+import javax.naming.Context
+import javax.naming.InitialContext
+import java.nio.file.DirectoryStream
+import java.nio.file.Files
+import java.nio.file.Path
+import grails.util.Environment
+import org.grails.config.NavigableMapPropertySource
+import org.grails.config.PropertySourcesConfig
+import org.grails.config.yaml.YamlPropertySourceLoader
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.MapPropertySource
+import org.springframework.core.io.DefaultResourceLoader
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+
+trait ExternallyConfigurable {
+    private ResourceLoader defaultResourceLoader = new DefaultResourceLoader()
+      YamlPropertySourceLoader yamlPropertySourceLoader = new YamlPropertySourceLoader()
+    private Logger log = LoggerFactory.getLogger('com.neilab.plugins.config.ApplicationConfig')
+    private String userHome = System.properties.getProperty('user.home')
+    private String separator = System.properties.getProperty('file.separator')
+
+    /**
+     * Returns the name of the prefix to be used for config file property names. If null is returned, the name returned
+     * from getExternalConfigKey will be used. e.g. A prefix of appPrefix would return config property names:
+     *
+     *  appPrefix.config
+     *  appPrefix.database.config
+     *  appPrefix.logging.config
+     *  appPrefix.external.config
+     *
+     * @return  the name to be used as the prefix for computed property names.
+     */
+    String getExternalConfigPrefix() {
+        return null
+    }
+
+    /**
+     *  Override to return the key to be used to obtain the prefix in the grails application config.
+     *  The default value is "info.app.name" which by default is configured as the application name.
+     *
+     * @return  The name of the key to be used to obtain the prefix
+     */
+
+    String getExternalConfigKey() {
+        return "info.app.name"
+    }
+
+    /**
+     *  Override to return the names of application environment entities to be used in retrieving a path
+     *  of application config files.
+     *
+     * @return  A list of Application Environment Entry Names
+     */
+    List<String> getExternalConfigEnvironmentNames() {
+        return ["CONFIG","EXTERNAL_CONFIG","LOGGING_CONFIG","DATABASE_CONFIG"]
+    }
+
+    void configureEnvironment(ConfigurableEnvironment environment) {
+
+        List locations = getLocations(environment)
+        String encoding = environment.getProperty('grails.config.encoding', String, 'UTF-8')
+
+        locations += variableNameList(environment)
+        locations += systemPropertyNameList(environment)
+
+        for (location in locations) {
+            MapPropertySource propertySource = null
+            Map currentProperties = getCurrentConfig(environment)
+            if (location instanceof Class) {
+                propertySource = loadClassConfig(location as Class,currentProperties)
+            } else {
+                // Replace placeholders from known locations
+                String finalLocation = environment.resolvePlaceholders(location as String)
+
+                Resource resource = defaultResourceLoader.getResource(finalLocation)
+                if (resource.exists()) {
+                    if (finalLocation.endsWith('.groovy')) {
+                        propertySource = loadGroovyConfig(resource, encoding, currentProperties)
+                    } else if (finalLocation.endsWith('.yml')) {
+                        environment.activeProfiles
+                        propertySource = loadYamlConfig(resource)
+                    } else {
+                        // Attempt to load the config as plain old properties file (POPF)
+                        propertySource = loadPropertiesConfig(resource)
+                    }
+                } else {
+                    log.debug("Config file {} not found", [finalLocation] as Object[])
+                }
+            }
+            if (propertySource?.getSource() && !propertySource.getSource().isEmpty()) {
+                environment.propertySources.addFirst(propertySource)
+            }
+        }
+    }
+
+    String getDefaultExternalConfigPrefix(ConfigurableEnvironment environment) {
+        String defaultConfigPrefix = this.externalConfigPrefix ?:
+                environment.getProperty(this.externalConfigKey,String,"app")
+        return defaultConfigPrefix
+    }
+
+    private List variableNameList(ConfigurableEnvironment environment) {
+        List locations = []
+
+        this.externalConfigEnvironmentNames.each {
+            try {
+                def external_config = ((Context)(new InitialContext().lookup("java:comp/env"))).lookup("${it}")
+
+                if(external_config) {
+                    log.info("Loading configuration: $external_config")
+                    locations <<  "${external_config}"
+                }
+            } catch (Exception e) {
+                log.debug("External configuration lookup failed: " + e)
+            }
+        }
+        return locations
+    }
+
+    private List systemPropertyNameList(ConfigurableEnvironment environment) {
+        List locations = []
+        String defaultConfigPrefix = this.getDefaultExternalConfigPrefix(environment)
+        [
+                defaultConfigPrefix,"database","logging","external"
+        ].each {
+            String configKey = it == defaultConfigPrefix ?
+                    "${defaultConfigPrefix}.config" : // appName.config
+                    "${defaultConfigPrefix}.${it}.config" // appName.[database,logging,exteral].config
+            String configPath = System.properties[configKey]
+            if (configPath) {
+                log.info("Loading configuration: $configPath")
+                locations << "file:" + configPath
+            }
+        }
+        return locations
+    }
+
+    // Resolve final locations, taking into account user home prefix and file wildcards
+    private List<Object> getLocations(ConfigurableEnvironment environment) {
+        List<Object> locations = environment.getProperty('grails.config.locations', List, []) as List<Object>
+        // See if grails.config.locations is defined in an environments block like 'development' or 'test'
+        String environmentString = "environments.${Environment.current.name}.grails.config.locations"
+        locations = environment.getProperty(environmentString, List, locations)
+        locations.collectMany { Object location ->
+            if(location instanceof CharSequence) {
+                location = replaceUserHomePrefix(location as String)
+                List<Object> expandedLocations = handleWildcardLocation(location as String)
+                if(expandedLocations) {
+                    return expandedLocations
+                }
+            }
+            return [location]
+        }
+    }
+
+    // Expands wildcards if any
+    List<Object> handleWildcardLocation(String location) {
+        if(location.startsWith('file:')) {
+            String locationFileName = location.tokenize(separator)[-1]
+            if(locationFileName.contains('*')) {
+                String parentLocation = location - locationFileName
+                try {
+                    Resource resource = defaultResourceLoader.getResource(parentLocation)
+                    if(resource.file.exists() && resource.file.isDirectory()) {
+                        Path dir = resource.file.toPath()
+                        DirectoryStream<Path> stream = Files.newDirectoryStream(dir, locationFileName)
+
+                        return stream.collect { Path p ->
+                            "file:${p.toAbsolutePath()}"
+                        } as List<Object>
+                    }
+                } catch (FileNotFoundException ignore) {
+                    return null
+                }
+
+            }
+        }
+        return null
+    }
+
+    // Replace ~ with value from system property 'user.home' if set
+    private String replaceUserHomePrefix(String location) {
+        if (userHome && location.startsWith('~/')) {
+            location = "file:${userHome}${location[1..-1]}"
+        }
+        return location
+    }
+    // Load groovy config from classpath
+    private MapPropertySource loadClassConfig(Class location, Map currentConfig) {
+        log.info("Loading config class {}", location.name)
+        ConfigSlurper slurper = new ConfigSlurper(Environment.current.name)
+        WriteFilteringMap filterMap = new WriteFilteringMap(currentConfig)
+        slurper.binding = filterMap
+        Map properties = slurper.parse((Class) location)?.flatten()
+        Map writtenValues = filterMap.getWrittenValues()
+        properties.putAll(writtenValues)
+        new MapPropertySource(location.toString(), properties)
+    }
+
+    // Load groovy config from resource
+    private MapPropertySource loadGroovyConfig(Resource resource, String encoding, Map currentConfig) {
+        log.info("Loading groovy config file {}", resource.URI)
+        String configText = resource.inputStream.getText(encoding)
+        ConfigSlurper slurper = new ConfigSlurper(Environment.current.name)
+        WriteFilteringMap filterMap = new WriteFilteringMap(currentConfig)
+        slurper.binding = filterMap
+        ConfigObject configObject = slurper.parse(configText)
+        Map<String, Object> properties = configText ? configObject?.flatten() as Map<String, Object> : [:]
+        Map writtenValues = filterMap.getWrittenValues()
+        properties.putAll(writtenValues)
+        new MapPropertySource(resource.filename, properties)
+    }
+
+    private NavigableMapPropertySource loadYamlConfig(Resource resource) {
+        log.info("Loading YAML config file {}", resource.URI)
+        NavigableMapPropertySource propertySource = yamlPropertySourceLoader.load(resource.filename, resource, null) //?.first() as NavigableMapPropertySource
+        return propertySource
+    }
+
+    private MapPropertySource loadPropertiesConfig(Resource resource) {
+        log.info("Loading properties config file {}", resource.URI)
+        Properties properties = new Properties()
+        properties.load(resource.inputStream)
+        new MapPropertySource(resource.filename, properties as Map)
+    }
+
+    static Map getCurrentConfig(ConfigurableEnvironment environment) {
+        return new PropertySourcesConfig(environment.propertySources)
+    }
+
+}
